0fc0929c9b3e2186b42425da4bfec89a65b3151a
Alper Alanyali <alanyalialper@gmail.com>
2023-06-17 18:04:41 +0300

    Job and JobCard is added

---
+++ src/pages/JobDetail/JobDetail-styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    title:{
+        marginTop:20,
+      fontSize:20,
+      fontWeight:'bold',
+      color:'dray'  ,
+      textAlign:'center',
+      marginBottom:20,
+    },
+    location_title:{
+        margin:10,
+        color:'#E97451',
+        fontWeight:'bold'
+    },
+    location_name:{
+        color:"black",
+        fontWeight:"bold"
+    },
+    row:{
+        flexDirection:'row'
+    },
+    html:{
+        margin:10,
+    }
+})
